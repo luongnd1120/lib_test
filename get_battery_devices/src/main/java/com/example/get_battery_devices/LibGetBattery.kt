6c5ac0f52121f259dd79ae741cb7b1dd6fbf559d
LUONGND1120\luong <luongnd1120@gmail.com>
2021-05-28 09:30:06 +0700

    first commit

---
+++ get_battery_devices/src/main/java/com/example/get_battery_devices/LibGetBattery.kt
@@ -0,0 +1,33 @@
+package com.example.get_battery_devices
+
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build
+
+class LibGetBattery {
+    fun getBatteryLevel(context: Context): Int {
+        val batteryLevel: Int
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val batteryManager = context.getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+        } else {
+            val intent = ContextWrapper(context).registerReceiver(
+                null,
+                IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+            )
+            batteryLevel =
+                intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(
+                    BatteryManager.EXTRA_SCALE,
+                    -1
+                )
+        }
+
+        return batteryLevel
+    }
+
+}
+
+
